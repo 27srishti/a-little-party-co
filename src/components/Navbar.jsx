cb3b283b91ba307faf3d96515195bf4f48c752b1
srishti suratkal <279srishti@gmail.com>
2025-11-27 16:15:34 +0530

    fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
                   <PartyPopper className="w-6 h-6 text-white" />
                 </div>
                 <span className="font-serif tracking-tight font-normal ">
-                  A Little Party Co
+                  A Little Party Co.
                 </span>
               </a>
             </div>
